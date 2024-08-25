3480935a802add5e8faf98ebc9bdea5e26c03021
ElinaNaghashyan <74202770+ElinaNaghashyan@users.noreply.github.com>
2024-08-25 18:11:05 +0400

    add structure with material design

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,10 @@
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+
 
 
 @NgModule({
@@ -8,11 +12,17 @@ import { NgModule } from '@angular/core';
 
   ],
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule
   ],
 
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule
   ],
 
 })
